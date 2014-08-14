97c7b14cf820f46438d2d8586cfd7bc01b93154a
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-08-13 19:58:32 -0600

    Button fades in & out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,23 +2,29 @@
 //= require jquery_ujs
 //= require_tree .
 
-var taskLists =jQuery('.task-list button');
+var taskLists = jQuery('.task-list button');
 
-taskLists.on('click', function(){
+taskLists.on('click', function () {
   var button = $(this);
   var ul = button.parents('.task-list').find('ul');
   ul.toggle();
 });
 
-$("button").click(function() {
-  $(this).html('Close');
-
-  $('button').mouseover(function () {
-    $(this).fadeTo('fast', 1);
-  });
-
-  $('.errors').clearTimeout(5000);
-
-
+$("button").on("click", function() {
+  var el = $(this);
+  if (el.text() == el.data("text-swap")) {
+    el.text(el.data("text-original"));
+  } else {
+    el.data("text-original", el.text());
+    el.text(el.data("text-swap"));
+  }
 });
 
+// $("button").mouseover(function () {
+ // $(this).fadeTo('fast', 1);
+// });
+
+
+// $('.errors').clearTimeout(5000);
+
+// });
